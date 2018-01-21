5ea422717b0cf9a1c4a7685cbee4e2af7d4455fc
rainstarmoon <1215884463@qq.com>
2018-01-21 20:55:25 +0800

    aaaa

---
+++ login/src/main/java/com/project/config/CustomFormAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.project.config;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+public class CustomFormAuthenticationFilter extends FormAuthenticationFilter{
+
+	@Override
+	protected boolean onAccessDenied(ServletRequest req, ServletResponse response) throws Exception {
+		HttpServletRequest request=(HttpServletRequest)req;
+		System.out.println(request.getServerName());
+		System.out.println(request.getServerPort());
+		System.out.println(request.getContextPath());
+		System.out.println(request.getServletPath());
+		System.out.println(request.getQueryString());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getRequestURL());
+		return super.onAccessDenied(req, response);
+	}
+	
+}
